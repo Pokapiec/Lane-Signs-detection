55ef2d7b220c3e6c4c263ced1718ad4dbb8bc2a8
Paweł Okapiec <pokapiec@gmail.com>
2021-05-15 13:19:11 +0200

    ORB changes

--- prototype.py
+++ prototype.py
@@ -1,5 +1,101 @@
 import cv2
 import numpy as np
+import os
+
+
+path = 'templates'
+orb = cv2.ORB_create(nfeatures=1000)
+
+images = []
+classNames = []
+listaTmp = os.listdir(path)
+
+for cl in listaTmp:
+    imgCur = cv2.imread(f'{path}/{cl}',0)
+    _, imgCur = cv2.threshold(imgCur,120,255,cv2.THRESH_BINARY)
+    images.append(imgCur)
+    classNames.append(os.path.splitext(cl)[0])
+
+
+
+def findDes(images):
+    desList = []
+    for img in images:
+        kp, des = orb.detectAndCompute(img,None)
+        desList.append(des)
+    return desList
+
+def findID(img, desList):
+    kp2, des2 = orb.detectAndCompute(img,None)
+    bf = cv2.BFMatcher()
+    matchList = []
+    finVal = -1
+    thres = 2
+    for des in desList:
+        matches = bf.knnMatch(des,des2,k=2)
+        good = []
+        for m,n in matches:
+            if m.distance < 0.75 * n.distance:
+                good.append([m])
+        matchList.append(len(good))
+
+    if len(matchList)!= 0:
+        if max(matchList) > thres:
+            finVal = matchList.index(max(matchList))
+    return finVal
+
+
+desList = findDes(images)
+
+img6 = cv2.imread("znak2.jpg")
+imgorig = img6.copy()
+
+
+img6 = cv2.cvtColor(img6,cv2.COLOR_BGR2GRAY)
+
+wys, szer = img6.shape[:2]
+
+maska = np.zeros(img6.shape[:2], np.uint8)
+maska[490:wys-200, 200:szer-400] = 255
+
+img6[maska!=255] = 0
+
+_, img6_th = cv2.threshold(img6,120,255,cv2.THRESH_BINARY)
+id = findID(img6_th,desList)
+
+if id != -1:
+    cv2.putText(imgorig,classNames[id],(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),1,cv2.LINE_AA) 
+
+
+cv2.imshow("orig", imgorig)
+cv2.waitKey(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -30,15 +126,32 @@ gray[maska!=255] = 0
 _, progt = cv2.threshold(tmp_gray,120,255,cv2.THRESH_BINARY)
 _, progs = cv2.threshold(gray,135,255,cv2.THRESH_BINARY)
 
-match = cv2.matchTemplate(progs, progt, cv2.TM_CCOEFF_NORMED)
-loc = np.where(match >= 0.32)
-# print(loc)
-# print("----------------")
-# print(list(zip(loc)))
-# print("----------------")
-# print(list(zip(*loc)))
-for pt in zip(*loc[::-1]):
-    cv2.rectangle(img2, pt, (pt[0] + szerokosc, pt[1] + wysokosc), (0,0,255), 3)
+
+# orb = cv2.ORB_create(nfeatures = 1000)
+
+# kp1, des1 = orb.detectAndCompute(progs,None)
+# kp2, des2 = orb.detectAndCompute(progt,None)
+
+# bf = cv2.BFMatcher()
+# matches = bf.knnMatch(des1,des2,k=2)
+
+# good = []
+# for m,n in matches:
+#     if m.distance < 0.75*n.distance:
+#         good.append([m])
+
+# img4 = cv2.drawMatchesKnn(img2,kp1,tmp,kp2,good,None)
+
+
+# match = cv2.matchTemplate(progs, progt, cv2.TM_CCOEFF_NORMED)
+# loc = np.where(match >= 0.32)
+# # print(loc)
+# # print("----------------")
+# # print(list(zip(loc)))
+# # print("----------------")
+# # print(list(zip(*loc)))
+# for pt in zip(*loc[::-1]):
+#     cv2.rectangle(img2, pt, (pt[0] + szerokosc, pt[1] + wysokosc), (0,0,255), 3)
 
 #bottom_right = (location[0] + szerokosc, location[1] + wysokosc)
 #cv2.rectangle(img,location,bottom_right,(0,0,255),5)
@@ -50,45 +163,34 @@ prog = cv2.adaptiveThreshold(gray,255,
 
 #cv2.morphologyEx(prog,cv2.MORPH_OPEN,np.ones((13,13),np.uint8))
 kontury = cv2.Canny(prog1,50,180)
-# lines = cv2.HoughLinesP(kontury,
-#                     rho = 1,
-#                     theta = np.pi/180,
-#                     threshold = 170,
-#                     minLineLength = 70,
-#                     maxLineGap = 100)
-lines = cv2.HoughLinesP(kontury,
-                    2,
-                    np.pi/180,
-                    170,
-                    np.array([]),
-                    minLineLength = 70,
-                    maxLineGap = 100)
-
-# for line in lines:
-#     x1, y1, x2, y2 = line[0]
-#     cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 5)
 
 
-cv2.circle(img, (620,500),5,(0,0,255),-1)
-cv2.circle(img, (790,500),5,(0,0,255),-1)
+# Tutaj sobie robiłem perspektywe w nadzieji o lepsze jutro
+# cv2.circle(img, (620,500),5,(0,0,255),-1)
+# cv2.circle(img, (790,500),5,(0,0,255),-1)
 
-cv2.circle(img, (250,700),5,(0,0,255),-1)
-cv2.circle(img, (1150,710),5,(0,0,255),-1)
+# cv2.circle(img, (250,700),5,(0,0,255),-1)
+# cv2.circle(img, (1150,710),5,(0,0,255),-1)
 
-pts1 = np.float32([[620,500],[790,500],[250,700],[1150,710]])
-pts2 = np.float32([[0,0],[600,0],[0,600],[600,600]])
+# pts1 = np.float32([[620,500],[790,500],[250,700],[1150,710]])
+# pts2 = np.float32([[0,0],[600,0],[0,600],[600,600]])
 
-matrix = cv2.getPerspectiveTransform(pts1,pts2)
+# matrix = cv2.getPerspectiveTransform(pts1,pts2)
 
-result = cv2.warpPerspective(img3,matrix,(600,600))
+# result = cv2.warpPerspective(img3,matrix,(600,600))
+
+# result_gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+# _, progP = cv2.threshold(result_gray,120,255,cv2.THRESH_BINARY)
+# kernel = np.ones((5,5),np.uint8)
+# opening = cv2.morphologyEx(progP, cv2.MORPH_ERODE, kernel)
+# edges_persp = cv2.Canny(opening,50,180)
 
 
-#print(match)
 #cv2.imshow("wsf",prog)
 #cv2.imshow("wsf1",kontury)
-cv2.imshow("img1",img)
-cv2.imshow("img",result)
+# cv2.imshow("img1",img)
 
-#cv2.imshow("img",match)
+# cv2.imshow("img",img4)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- vision.py
+++ vision.py
@@ -1,26 +1,51 @@
-# Algorytm SURF, chyba jest platny i licencja kosztuje xD
-
-# img = cv2.imread("znak1.jpg",0)
-
-# surf = cv2.xfeatures2d.SIFT_create()
-
-# kp, des = surf.detectAndCompute(img,None)
-# img2 = cv2.drawKeypoints(img,kp,None,(255,0,0),4)
-# cv2.imshow("a",img2)
-
-# # Okrągle znaki na ulicach ???
-# circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,20,
-#                             param1=50,param2=30,minRadius=0,maxRadius=0)
-# circles = np.uint16(np.around(circles))
-
-# for i in circles[0,:]:
-#     # draw the outer circle
-#     cv2.circle(edges,(i[0],i[1]),i[2],(0,255,0),2)
-#     # draw the center of the circle
-#     cv2.circle(edges,(i[0],i[1]),2,(0,0,255),3)
 import cv2
 import numpy as np
 import math
+import os
+
+
+# path = 'templates'
+# orb = cv2.ORB_create(nfeatures=1000)
+
+# images = []
+# classNames = []
+# listaTmp = os.listdir(path)
+
+# for cl in listaTmp:
+#     imgCur = cv2.imread(f'{path}/{cl}',0)
+#     _, imgCur = cv2.threshold(imgCur,120,255,cv2.THRESH_BINARY)
+#     images.append(imgCur)
+#     classNames.append(os.path.splitext(cl)[0])
+
+
+
+# def findDes(images):
+#     desList = []
+#     for img in images:
+#         kp, des = orb.detectAndCompute(img,None)
+#         desList.append(des)
+#     return desList
+
+# def findID(img, desList):
+#     kp2, des2 = orb.detectAndCompute(img,None)
+#     bf = cv2.BFMatcher()
+#     matchList = []
+#     finVal = -1
+#     thres = 2
+#     for des in desList:
+#         matches = bf.knnMatch(des,des2,k=2)
+#         good = []
+#         for m,n in matches:
+#             if m.distance < 0.75 * n.distance:
+#                 good.append([m])
+#         matchList.append(len(good))
+
+#     if len(matchList)!= 0:
+#         if max(matchList) > thres:
+#             finVal = matchList.index(max(matchList))
+#     return finVal
+
+# ------------------------------------------------------------------------------------------
 
 video = cv2.VideoCapture("znaki_vid.mp4")
 tmp = cv2.imread("znaczek50.jpg",0)
@@ -41,12 +66,30 @@ while True:
     gray1 = cv2.cvtColor(orig_frame, cv2.COLOR_BGR2GRAY)
     h,w = gray1.shape[:2]
     #Trojkat
-    region_of_interest_vertices = [(w/6, h - h/4.5),(w / 2.4, h / 2),(w-w/4, h-h/4.5)]
+    trojkat1 = [(w/6, h - h/4.5),(w / 2.4, h / 2),(w-w/4, h-h/4.5)]
     mask = np.zeros_like(gray1)
-    cv2.fillPoly(mask,np.array([region_of_interest_vertices], np.int32), 255)
+    cv2.fillPoly(mask,np.array([trojkat1], np.int32), 255)
     gray1 = cv2.bitwise_and(gray1, mask)
 
-    # Rozpoznawanie znaków na drodze
+    img_thr2 = cv2.adaptiveThreshold(gray1,255,
+            cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+            cv2.THRESH_BINARY,71,-20)
+
+    # Proba wykrywania znaku za pomoca cech ORB
+
+    # trojkat2 = [(w/3, h - h/4),(w / 2.4, h / 2),(w-w/2.5, h-h/4)]
+    # mask2 = np.zeros_like(gray1)
+    # cv2.fillPoly(mask2,np.array([trojkat2], np.int32), 255)
+    # img_thr2 = cv2.bitwise_and(img_thr2, mask2)
+    # desList = findDes(images)
+    # id = findID(img_thr2,desList)
+
+    # if id != -1:
+    #     cv2.putText(orig_frame,classNames[id],(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),1,cv2.LINE_AA)
+
+
+
+    #Rozpoznawanie znaków na drodze
 
     _, progt = cv2.threshold(tmp,120,255,cv2.THRESH_BINARY)
     _, progs = cv2.threshold(gray1,135,255,cv2.THRESH_BINARY)
@@ -56,8 +99,9 @@ while True:
     loc = np.where(match >= 0.35)
     for pt in zip(*loc[::-1]):
         cv2.rectangle(orig_frame, pt, (pt[0] + szerokosc, pt[1] + wysokosc), (0,0,255), 3)
+        cv2.putText(orig_frame,"Znaczek 50 na godz",(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),1,cv2.LINE_AA)
 
-    # _, thresh = cv2.threshold(gray,120,255,cv2.THRESH_BINARY)
+    _, thresh = cv2.threshold(gray,120,255,cv2.THRESH_BINARY)
 
     thresh = cv2.adaptiveThreshold(gray,255,
             cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
@@ -95,14 +139,35 @@ while True:
                     maxLineGap = 40)
 
 
+
+    # cv2.circle(orig_frame, (440,380),5,(0,0,255),-1)
+    # cv2.circle(orig_frame, (610,380),5,(0,0,255),-1)
+
+    # cv2.circle(orig_frame, (250,500),5,(0,0,255),-1)
+    # cv2.circle(orig_frame, (900,500),5,(0,0,255),-1)
+
+    # pts1 = np.float32([[440,380],[610,380],[250,500],[900,500]])
+    # pts2 = np.float32([[0,0],[600,0],[0,600],[600,600]])
+    # matrix = cv2.getPerspectiveTransform(pts1,pts2)
+    # result = cv2.warpPerspective(orig_frame,matrix,(600,600))
+
+    # result_gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+    # _, progP = cv2.threshold(result_gray,120,255,cv2.THRESH_BINARY)
+    # kernel = np.ones((5,5),np.uint8)
+    # opening = cv2.morphologyEx(progP, cv2.MORPH_ERODE, kernel)
+    # edges_persp = cv2.Canny(opening,50,180)
+
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line[0]
             slope = (y2 - y1) / (x2 - x1)
             if math.fabs(slope) > 0.4:
                 cv2.line(orig_frame, (x1, y1), (x2, y2), (0, 255, 0), 4)
+
+    #v = np.hstack((gray,edges))
     cv2.imshow("frame", orig_frame)
-    cv2.imshow("edges", gray1)
+    cv2.imshow("edges", edges)
+
     key = cv2.waitKey(1)
     if key == 27:
         break
